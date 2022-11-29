53f82e5e04e918d8eeeb1ce2a62ee83f33291c2b
Daniela <Daniela-Haeussler@gmx.de>
2022-11-29 12:19:18 +0100

    Constructuren, Method der Shape.java

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,7 +3,7 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+   
     }
 
     public static void output(String outputStr) {
--- com/cc/java/Rectangle.java
+++ com/cc/java/Rectangle.java
@@ -1,5 +1,18 @@
 package com.cc.java;
 
-public class Rectangle {
+public class Rectangle extends Shape {
+
+    private double a, b;
+
+    public Rectangle(double a, double b){
+        this.a=a;
+        this.b=b;
+    }
+    @Override
+    public double area() {
+        return a*b;
+    };
+
+
     
 }

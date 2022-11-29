30e25bcd06e48e49632604131a7fd07a028346e5
Daniela <Daniela-Haeussler@gmx.de>
2022-11-29 12:26:07 +0100

    Methodcalls

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,7 +3,12 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-   
+Rectangle rectangle=new Rectangle(5, 10);
+System.out.println("Fläche Rechteck: " + rectangle.area());
+Circle circle=new Circle(25);
+System.out.println("Fläche Kreis: "+ circle.area());
+Triangle triangle= new Triangle(3, 15);
+System.out.println("Fläche Dreieck: "+ triangle.area()); 
     }
 
     public static void output(String outputStr) {

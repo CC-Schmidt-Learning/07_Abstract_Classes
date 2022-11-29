4d7f17c59073d1f4985a41b597408b143ac1860e
Daniela <Daniela-Haeussler@gmx.de>
2022-11-29 12:33:42 +0100

    new output

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -8,8 +8,15 @@ System.out.println("Fläche Rechteck: " + rectangle.area());
 Circle circle=new Circle(25);
 System.out.println("Fläche Kreis: "+ circle.area());
 Triangle triangle= new Triangle(3, 15);
-System.out.println("Fläche Dreieck: "+ triangle.area()); 
+System.out.println("Fläche Dreieck: "+ triangle.area());
+output("------  Differenzen ------");
+output("Diff. r-t: " + areaDiff(rectangle, triangle)); 
     }
+
+    private static double areaDiff(Rectangle r, Triangle t) {
+
+        return r.area() - t.area();
+     }
 
     public static void output(String outputStr) {
         System.out.println(outputStr);
